a1a7d5f9aa8a718e2a9c0b658b81f695c1e1b26c
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-26 00:56:42 -0600

    Made minor modification to About Me page

--- ror/client/src/components/About.js
+++ ror/client/src/components/About.js
@@ -4,7 +4,9 @@ function About() {
   
   return (
     <Container>
-      <p>Hi, my name is Josh Holmes! I'm a full-stack software engineer and I built this app. This application serves as a template for creating a Docker image out of a React on Rails application with a PostgreSQL database on the backend. For full instructions on how to use this, visit my <a href="https://github.com/joshua-holmes/docker-template-react-on-rails" target="_blank">GitHub page on this project</a>!</p>
+      <h2>Who Am I Again?</h2>
+      <p>Hi, my name is Joshua Holmes! I'm a full-stack software engineer and I built this app. This application serves as a template for creating a Docker image out of a React on Rails application with a PostgreSQL database on the backend. For full instructions on how to use this, visit my <a href="https://github.com/joshua-holmes/docker-template-react-on-rails" target="_blank">GitHub page on this project</a>!</p>
+      <p>If you are here to use the todo list, don't let me stop you!</p>
     </Container>
   )
 }
